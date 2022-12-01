d841b37021dcef719fa9cf92b22b0d00f00bbcbc
晒晕的兔子 <gubu1986@126.com>
2022-12-01 18:27:01 +0800

    Initial Commit

---
+++ 404x/_04xApp.swift
@@ -0,0 +1,17 @@
+//
+//  _04xApp.swift
+//  404x
+//
+//  Created by 顾艳华 on 2022/12/1.
+//
+
+import SwiftUI
+
+@main
+struct _04xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
